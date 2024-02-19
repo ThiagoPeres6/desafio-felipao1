61c1517956637c7098a39042cf87e237742957db
ThiagoPeres6 <thiago.s.peres@hotmail.com>
2024-02-18 21:44:32 -0300

    Primeiro Projeto do Felipão da DIO

---
+++ index.js
@@ -0,0 +1,23 @@
+let heroi = "Peth Siper"
+let xp = 20000
+
+if(xp<=1000){
+    console.log("O Herói de nome " + heroi + " está no nível de Ferro");
+}else if(xp<=2000){
+    console.log("O Herói de nome " + heroi + " está no nível de Bronze");
+}else if(xp<=5000){
+    console.log("O Herói de nome " + heroi + " está no nível de Prata");
+}else if(xp<=7000){
+    console.log("O Herói de nome " + heroi + " está no nível de Ouro");
+}else if(xp<=8000){
+    console.log("O Herói de nome " + heroi + " está no nível de Platina");
+}else if(xp<=9000){
+    console.log("O Herói de nome " + heroi + " está no nível de Ascendente");
+}else if(xp<=10000){
+    console.log("O Herói de nome " + heroi + " está no nível de Imortal");
+}else{
+    console.log("O Herói de nome " + heroi + " está no nível de Radiante");
+}
+
+
+
